ce65158e3527a19da0fd0a33219b54e6836f55ee
xProMDFKx <37838961+xProMDFKx@users.noreply.github.com>
2018-03-28 17:25:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,29 +1,37 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const prefix = '/';
-client.on('ready', () => {
-    console.log('I am ready!');
-});
-
-//SI
+const embed = new Discord.RichEmbed()
+const prefix = "/"
 client.on('message', message => {
-    if (message.content === '/serverinfo') {
+  let args = message.content.split(" ").slice(1);
+
+//SAY
+  if(message.content.startsWith(prefix + "say"))  {
+         message.delete()
+         const embed = new Discord.RichEmbed()
+         .setDescription(args.join(" "))
+         .setColor(0xff0000)
+    message.channel.sendEmbed(embed);
+ 
+     }
+//SERVERINFO
+if (message.content.startsWith(prefix + 'serverinfo')) {
   const embed = new Discord.RichEmbed()
   embed.addField('Members', message.guild.memberCount, true)
   embed.addField('Name', message.guild.name, true)
   embed.addField('Region', message.guild.region, true)
   embed.addField('Owner', message.guild.owner, true)
   embed.addField('ID', message.guild.id, true)
-  embed.setColor(`ff0000`)
+  embed.setColor(ff0000)
   embed.setThumbnail(message.guild.iconURL)
   message.channel.sendEmbed(embed)
-   }
-
-client.on('message', message => {
-    if (message.content === '/help') {
-    	message.reply('pong');
-   }
+}
 });
 
-// THIS  MUST  BE  THIS  WAY
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
+ 
 client.login(process.env.BOT_TOKEN);
