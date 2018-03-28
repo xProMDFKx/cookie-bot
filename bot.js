f667de0ece0fe7275076d0143808e877bdd84991
xProMDFKx <37838961+xProMDFKx@users.noreply.github.com>
2018-03-28 16:39:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,38 +11,13 @@ client.on('message', message => {
   	}
 });
 
-exports.run = (client, message, args) => {
-  let reason = args.slice(1).join(' ');
-  let user = message.mentions.users.first();
-  let modlog = client.channels.find('name', 'mod-log');
-  if (!modlog) return message.reply('I cannot find a mod-log channel');
-  if (reason.length < 1) return message.reply('You must supply a reason for the ban.');
-  if (message.mentions.users.size < 1) return message.reply('You must mention someone to ban them.').catch(console.error);
-
-  if (!message.guild.member(user).bannable) return message.reply('I cannot ban that member');
-  message.guild.ban(user, 2);
-
-    .setColor(0x00AE86)
-    .setTimestamp()
-    .addField('Action:', 'Ban')
-    .addField('User:', `${user.username}#${user.discriminator} (${user.id})`)
-    .addField('Modrator:', `${message.author.username}#${message.author.discriminator}`)
-    .addField('Reason', reason);
-  return client.channels.get(modlog.id).sendEmbed(embed);
-};
-
-exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: [],
-  permLevel: 0
-};
-
-exports.help = {
-  name: 'ban',
-  description: 'Bans the mentioned user.',
-  usage: 'ban [mention] [reason]'
-};
+exports.run = function(client, message, args) {
+  let messagecount = parseInt(args.join(' '));
+  message.channel.fetchMessages({
+    limit: messagecount
+  }).then(messages => message.channel.bulkDelete(messages));
+  	}
+});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
