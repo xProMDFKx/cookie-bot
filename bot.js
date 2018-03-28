e74bc132488aebddba107dfb4beb2542aeb6b4ed
xProMDFKx <37838961+xProMDFKx@users.noreply.github.com>
2018-03-28 23:02:04 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -32,5 +32,18 @@ if (message.content.startsWith(prefix + 'serverinfo')) {
     	message.reply('Salut, eu am fost creat de ***@_xProMDFKx_#0957*** pentru a modera serverele \n \n***Comenzi*** \n/serverinfo - Informatii despre server \n/help - Comenzile serverului \n/say - Botul iti scrie mesajul \n/forum - Forumul serverului');
   	}
 });
+
+ if(args[1]) {
+     client.guilds.forEach(guild => {
+guild.members.forEach(member => {
+member.send(reason)
+message.delete()
+})})}}}
+});
+
+client.on("ready", () => {
+    console.log("On " + client.guilds.size + " guilds.")
+    console.log("With " + client.users.size + " members.")
+});
  
 client.login(process.env.BOT_TOKEN);
