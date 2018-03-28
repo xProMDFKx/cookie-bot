3eb8af5d44d3a698536e584967d8576b07c6d1be
xProMDFKx <37838961+xProMDFKx@users.noreply.github.com>
2018-03-28 23:20:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,9 +33,22 @@ if (message.content.startsWith(prefix + 'serverinfo')) {
     }
 });
 
+client.on(`message`, msg => {
+const memelink = [""]
+
+
+if (msg.content.startsWith(`-memes`)) {
+   msg.channel.send({file: memelink[Math.floor(Math.random() * memelink.length)]}).catch(err => {console.log(err)})
+}
+})
+
+client.on('ready', () => {
+  client.user.setGame('-cookies-network.org/forum', 'https://www.twitch.tv/streamerhouse')
+})
+
 client.on('message', message => {
 if(message.content.startsWith('-mass')) {
-    if(message.author.id === "419952579812786186"){
+    if(message.author.id === "295952120400838657"){
         let args = message.content.split(" ").slice(1);
         var argresult = args.join(" ")
         const argsresult = args.join(" ")
