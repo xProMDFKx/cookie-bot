ea8cc58520437da88286826adf6a0b75b677944a
xProMDFKx <37838961+xProMDFKx@users.noreply.github.com>
2018-03-29 07:39:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,6 +30,11 @@ if (message.content.startsWith(prefix + 'serverinfo')) {
 //SCA
     if (message.content === 'ping') {
     	message.reply('Salut, eu am fost creat de ***@_xProMDFKx_#0957*** pentru a modera serverele \n \n***Comenzi*** \n/serverinfo - Informatii despre server \n/help - Comenzile serverului \n/say - Botul iti scrie mesajul \n/forum - Forumul serverului');
+      
+    }  
+//SFSA
+    if (message.content === 'ping') {
+    	message.reply('<User> Pong! (took: ${msg.createdTimestamp - message.createdTimestamp}ms)');
     }
 });
 
