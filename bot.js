57dd5334b2821468f04eba7f000d0e917b2e9222
xProMDFKx <37838961+xProMDFKx@users.noreply.github.com>
2018-03-31 15:41:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -59,7 +59,7 @@ if (message.content.startsWith(prefix + 'serverinfo')) {
       return message.reply("Please indicate a reason for the kick!");
     
     // Now, time for a swift kick in the nuts!
-    await member.kick(reason)
+    member.kick(reason)
       .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
     message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
     }
