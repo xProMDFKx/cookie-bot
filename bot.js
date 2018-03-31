fdd9844d8ccbc93f37f23eafd4be93c22bb7b5be
xProMDFKx <37838961+xProMDFKx@users.noreply.github.com>
2018-03-31 15:19:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,6 +35,23 @@ if (message.content.startsWith(prefix + 'serverinfo')) {
 //SFSA
     if (message.content === 'pinsg') {
     	message.reply('<User> Pong! (took: ${msg.createdTimestamp - message.createdTimestamp}ms)');
+      
+    }
+//SCAS
+  if(command === "purge") {
+    // This command removes all messages from all users in the channel, up to 100.
+    
+    // get the delete count, as an actual number.
+    const deleteCount = parseInt(args[0], 10);
+    
+    // Ooooh nice, combined conditions. <3
+    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+    
+    // So we get our messages, and delete them. Simple enough, right?
+    const fetched = await message.channel.fetchMessages({count: deleteCount});
+    message.channel.bulkDelete(fetched)
+      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
     }
 });
 
