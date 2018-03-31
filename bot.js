23ba74bd90c9fee5480c1c5eae5e44b14b60e7fd
xProMDFKx <37838961+xProMDFKx@users.noreply.github.com>
2018-03-31 16:18:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,18 +38,18 @@ if (message.content.startsWith(prefix + 'serverinfo')) {
       
     }
 //SFSA
-    if (message.content == "kick") { // creates the command kick
+    if (command == "mute") { // creates the command mute
         if (!message.member.roles.some(r=>["Administrator"].includes(r.name)) ) return message.reply("Sorry, you do not have the permission to do this!"); // if author has no perms
-        var kickedmember = message.mentions.members.first(); // sets the mentioned user to the var kickedmember
-        if (!kickedmember) return message.reply("Please mention a valid member of this server!") // if there is no kickedmmeber var
-        if (!kickedmember.kickable) return message.reply("I cannot kick this member!") // if the member is unkickable
-        var kickreasondelete = 10 + kickedmember.user.id.length //sets the length of the kickreasondelete
-        var kickreason = message.content.substring(kickreasondelete).split(" "); // deletes the first letters until it reaches the reason
-        var kickreason = kickreason.join(" "); // joins the list kickreason into one line
-        if (!kickreason) return message.reply("Please indicate a reason for the kick!") // if no reason
-        kickedmember.kick(kickreason) //if reason, kick
-            .catch(error => message.reply(`Sorry @${message.author} I couldn't kick because of : ${error}`)); //if error, display error
-        message.reply(`${kickedmember.user.username} has been kicked by ${message.author.username} because: ${kickreason}`); // sends a message saying he was kicked
+        var mutedmember = message.mentions.members.first(); // sets the mentioned user to the var kickedmember
+        if (!mutedmember) return message.reply("Please mention a valid member of this server!") // if there is no kickedmmeber var
+        if (mutedmember.hasPermission("ADMINISTRATOR")) return message.reply("I cannot mute this member!") // if memebr is an admin
+        var mutereasondelete = 10 + mutedmember.user.id.length //sets the length of the kickreasondelete
+        var mutereason = message.content.substring(mutereasondelete).split(" "); // deletes the first letters until it reaches the reason
+        var mutereason = mutereason.join(" "); // joins the list kickreason into one line
+        if (!mutereason) return message.reply("Please indicate a reason for the mute!") // if no reason
+        mutedmember.addRole(mutedrole) //if reason, kick
+            .catch(error => message.reply(`Sorry ${message.author} I couldn't mute because of : ${error}`)); //if error, display error
+        message.reply(`${mutedmember.user} has been muted by ${message.author} because: ${mutereason}`); // sends a message saying he was kicked
     }
 });
 
