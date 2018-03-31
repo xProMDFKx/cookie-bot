ca828ed4fe145f02aea0beee32eae121868b55ff
xProMDFKx <37838961+xProMDFKx@users.noreply.github.com>
2018-03-31 15:33:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,20 +38,25 @@ if (message.content.startsWith(prefix + 'serverinfo')) {
       
     }
 //SCAS
-    if(command === 'purge') {
-    // This command removes all messages from all users in the channel, up to 100.
+  if (message.content === "ban") {
+    // Most of this command is identical to kick, except that here we'll only let admins do it.
+    // In the real world mods could ban too, but this is just an example, right? ;)
+    if(!message.member.roles.some(r=>["Administrator"].includes(r.name)) )
+      return message.reply("Sorry, you don't have permissions to use this!");
     
-    // get the delete count, as an actual number.
-    const deleteCount = parseInt(args[0], 10);
+    let member = message.mentions.members.first();
+    if(!member)
+      return message.reply("Please mention a valid member of this server");
+    if(!member.bannable) 
+      return message.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
+
+    let reason = args.slice(1).join(' ');
+    if(!reason)
+      return message.reply("Please indicate a reason for the ban!");
     
-    // Ooooh nice, combined conditions. <3
-    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
-    
-    // So we get our messages, and delete them. Simple enough, right?
-    const fetched = message.channel.fetchMessages({count: deleteCount});
-    message.channel.bulkDelete(fetched)
-      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+    await member.ban(reason)
+      .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
+    message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
     }
 });
 
