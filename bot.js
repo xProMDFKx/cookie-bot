cb12b359ef034454ed151930d4e23ae37d23e969
xProMDFKx <37838961+xProMDFKx@users.noreply.github.com>
2018-03-31 16:27:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,23 +38,26 @@ if (message.content.startsWith(prefix + 'serverinfo')) {
       
     }
 //SFSA
-    if (message.content === "purge") {
-        let messagecount = parseInt(args[1]) || 1;
-
-        var deletedMessages = -1;
-
-        message.channel.fetchMessages({limit: Math.min(messagecount + 1, 100)}).then(messages => {
-            messages.forEach(m => {
-                if (m.author.id == bot.user.id) {
-                    m.delete().catch(console.error);
-                    deletedMessages++;
-                }
-            });
-        }).then(() => {
-                if (deletedMessages === -1) deletedMessages = 0;
-                message.channel.send(`:white_check_mark: Purged \`${deletedMessages}\` messages.`)
-                    .then(m => m.delete(2000));
-        }).catch(console.error);
+    if (message.content === "heslp") { // creates a command *help
+        var embedhelpmember = new Discord.RichEmbed() // sets a embed box to the variable embedhelpmember
+            .setTitle("**List of Commands**\n") // sets the title to List of Commands
+            .addField(" - help", "Displays this message (Correct usage: *help)") // sets the first field to explain the command *help
+            .addField(" - info", "Tells info about myself :grin:") // sets the field information about the command *info
+            .addField(" - ping", "Tests your ping (Correct usage: *ping)") // sets the second field to explain the command *ping
+            .addField(" - cookie", "Sends a cookie to the desired player! :cookie: (Correct usage: *cookie @username)") // sets the third field to explain the command *cookie
+            .addField(" - 8ball", "Answers to all of your questions! (Correct usage: *8ball [question])") // sets the field to the 8ball command
+            .setColor(0xFFA500) // sets the color of the embed box to orange
+            .setFooter("You need help, do you?") // sets the footer to "You need help, do you?"
+        var embedhelpadmin = new Discord.RichEmbed() // sets a embed box to the var embedhelpadmin
+            .setTitle("**List of Admin Commands**\n") // sets the title
+            .addField(" - say", "Makes the bot say whatever you want (Correct usage: *say [message])")
+            .addField(" - mute", "Mutes a desired member with a reason (Coorect usage: *mute @username [reason])") // sets a field
+            .addField(" - unmute", "Unmutes a muted player (Correct usage: *unmute @username)")
+            .addField(" - kick", "Kicks a desired member with a reason (Correct usage: *kick @username [reason])") //sets a field
+            .setColor(0xFF0000) // sets a color
+            .setFooter("Ooo, an admin!") // sets the footer
+        message.channel.send(embedhelpmember); // sends the embed box "embedhelpmember" to the chatif
+        if(message.member.roles.some(r=>["bot-admin"].includes(r.name)) ) return message.channel.send(embedhelpadmin); // if member is a botadmin, display this too
     }
 });
 
