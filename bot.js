275ada3d95403322beb6f15f553230d4d0a8c5f8
xProMDFKx <37838961+xProMDFKx@users.noreply.github.com>
2018-03-31 16:59:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -42,9 +42,8 @@ if (message.content.startsWith(prefix + 'serverinfo')) {
     if (message.content === "/forum") { // creates the command cookie
   const embed = new Discord.RichEmbed()
   embed.setTitle("**Forum**\n")
-  embed.addField('Cookie-NetWork.Org/Forum', message, true)
+  embed.addField('Cookie-NetWork.Org/Forum', message.Join, true)
   embed.setColor(0xFF8B00)
-  embed.addField('Nu uita sa te inregistrezi', message.guild.iconURL, true)
   message.channel.sendEmbed(embed)
       
     }
